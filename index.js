76ee621338ecda60914a480681fb2ab182c9d583
dandickout <dan@carbonblock.io>
2022-10-24 15:01:42 -0400

    Rewrote parameter validation and added examples for Mongo operations as well as deepmerge for document merging.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Imports
 const MongoClient = require("mongodb").MongoClient;
-
+const merge = require('deepmerge');
   
 //Get environmental variables
-let MONGODB_URI = process.env.MONGO_URL;
+let MONGODB_URI = process.env.MONGO_URI;
 let MONGODB = process.env.MONGODB;
+let MONGODB_COLL = process.env.MONGODB_COLL;
 
 // Once we connect to the database once, we'll store that connection and reuse it so that we don't have to connect to the database on every request.
 let cachedDb = null;
@@ -53,7 +54,7 @@ exports.handler = async (event, context) => {
   catch (error) {
     const errorString = "Error connection to database " + error.stack;
     console.error(errorString);
-    response = {
+    let response = {
             statusCode: 500,
             body: JSON.stringify(errorString)
         };
@@ -62,18 +63,48 @@ exports.handler = async (event, context) => {
 
   // ++ Parse incoming variables ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   try {
+
+    //parse event body and deconstruct into variables
+    let data = JSON.parse(event.body);
+    //parse individual sections or nothing if no section was sent
+    let {cust_org_data} = data || {};
+    let {cust_org_prefs} = data || {};
+    let {audit_trail} = data || {};
+    let {user_id, cust_org_id} = data || {};
+    let {cust_org_name, cust_org_email, cust_org_phone} = cust_org_data || {};
+
+    body = {
+      cust_org_id: cust_org_id,
+      user_id : user_id,
+      cust_org_data: cust_org_data,
+      cust_org_prefs: cust_org_prefs,
+      audit_trail: audit_trail
+    }
+
     switch (event.httpMethod) {
 
-      case 'DELETE':
+      case 'POST':
+            //these are all required for post
+            if(!user_id || !cust_org_id || !cust_org_name || !cust_org_email || !cust_org_phone) {
+              throw new Error(body.error = 'Missing required parameter')}
           break;
 
       case 'GET':
-          break;
-
-      case 'POST':
+            //either of these can be used for
+            if(!user_id && !cust_org_id) {
+              throw new Error(body.error = 'Missing required parameter')}
           break;
 
       case 'PUT':
+            //these are all optional for put
+            if(!user_id || !cust_org_id) {
+              throw new Error(body.error = 'Missing required parameter')}
+          break;
+
+      case 'DELETE':
+            //Required
+            if(!user_id || !cust_org_id) {
+              throw new Error(body.error = 'Missing required parameter')}
           break;
 
       default:
@@ -89,23 +120,67 @@ exports.handler = async (event, context) => {
 
   // ++ Process Prep ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+  const date = new Date(Date.now());
+  //create template doc with placeholders for each section
+  let template_doc = {
+    cust_org_id: cust_org_id,
+    user_id : user_id,
+    cust_org_data: cust_org_data || {},
+    cust_org_prefs: cust_org_prefs || {},
+    audit_trail: audit_trail || {}
+  };
 
 
   // ++ Process method ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   try {
     switch (event.httpMethod) {
-        case 'DELETE':
-            body = await db.collection(somecollection).deleteOne(singledocument).promise();
-            break;
-        case 'GET':
-            body = await db.collection(somecollection).find(query).promise();
-            break;
-        case 'POST':
-            body = await db.collection(somecollection).insertOne(document).promise();
-            break;
-        case 'PUT':
-            body = await db.collection(somecollection).updateOne(document).promise();
-            break;
+      case 'DELETE':
+          body = await db.collection(MONGODB_COLL).deleteOne({"cust_org_id": cust_org_id});
+        break;
+
+      case 'GET':
+          if(cust_org_id){
+            body = await db.collection(MONGODB_COLL).findOne({"cust_org_id": cust_org_id});
+          }
+          else if(user_id){
+            body = await db.collection(MONGODB_COLL).findOne({"user_id": user_id});
+          }
+        break;
+
+    case 'POST':
+          //Add initial onbaording stage
+          template_doc.onboarding_stage = 1;
+          //Add default preferences
+          template_doc.cust_org_prefs = {
+            emissions_output_units: "kg"
+          }
+          //get the current Date as ISO and log who and when record was created
+          template_doc.audit_trail = {
+            created_at: date.toISOString(),
+            created_by: user_id
+          }
+
+          //insert the new document
+          body = await db.collection(MONGODB_COLL).insertOne(template_doc);
+        break;
+
+    case 'PUT':
+          //get the existing document to update only new or changed sections
+          old_doc = await db.collection(MONGODB_COLL).findOne({"cust_org_id": cust_org_id});
+          //get the current Date as ISO and log who and when record was updated
+          template_doc.audit_trail = {
+            update_at: date.toISOString(),
+            updated_by: user_id
+          }
+
+          //deepmerge the template with the existing doc
+          new_doc = merge(old_doc, template_doc);
+          delete new_doc['_id'];
+
+          //update all fields in the document
+          body = await db.collection(MONGODB_COLL).updateOne({"cust_org_id": cust_org_id}, {$set: new_doc});
+        break;
+
         default:
             throw new Error(`Unsupported method "${event.httpMethod}"`);
     }
